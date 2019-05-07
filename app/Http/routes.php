3e199c1cb09fd919a132338bed6eb22af9f46118
EC2 Default User <ec2-user@ip-172-31-83-114.ec2.internal>
2019-05-07 13:19:44 +0000

    06 commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,6 +26,20 @@ Route::group(['middleware' => ['web']], function() {
 	    ]);
 	});
 	Route::post('/book', function(Request $request) {
+	    $validator = Validator::make($request->all(), [
+			'name' => 'required|max:255',
+		]);
+		if($validator->fails()) {
+			return redirect('/')
+				->withInput()
+				->withErrors($validator);
+		}
+		$book =	new Book;
+		$book->title = $request->name;
+		$book->save();
+		
+		return redirect('/');
+	    
 	});
 	Route::delete('/book/{book}', function(Book $book) {
 	});
